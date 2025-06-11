f8eca9683d9cfd2b61dc856d24d93b3738e7cd6d
Hugo Muñoz <munozhugo46@gmail.com>
2025-06-11 04:36:57 -0300

    railwal

--- config/database.js
+++ config/database.js
@@ -3,11 +3,11 @@ const mysql2= require('mysql2');
 const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
+    process.env.MYSQLDATABASE,
+    process.env.MYSQLUSER,
+    process.env.MYSQLPASSWORD, {
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
     dialect: 'mysql',
     dialectModule: mysql2,
     logging: false,
